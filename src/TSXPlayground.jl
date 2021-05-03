6b7ce13f71233aa1711065fa8214b18e59265198
Takafumi Arakaki <aka.tkf@gmail.com>
2021-05-02 23:47:46 -0400

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.16

---
+++ src/TSXPlayground.jl
@@ -0,0 +1,5 @@
+module TSXPlayground
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using TSXPlayground
+using Test
+
+@testset "TSXPlayground.jl" begin
+    # Write your tests here.
+end
